2aafe21049279fe14b569bfa2053c8a40b0444a5
thunderhead27 <thunderhead27@gmail.com>
2022-10-19 15:37:33 -0700

    responsive fix

--- components/card.js
+++ components/card.js
@@ -29,7 +29,7 @@ export default function Card() {
 
     const SelectButton = () => {
       return (
-        <div tw="flex flex-row justify-between mb-8">
+        <div tw="flex flex-row justify-between sm:mb-10">
           {numbers.map(number => (
             <Button
               active={active === number}
@@ -49,11 +49,13 @@ export default function Card() {
 
     return (
       <>
-        <div tw="h-10 w-10 rounded-full bg-dBlue flex items-center justify-center mb-[30px]">
+        <div tw="h-10 w-10 rounded-full bg-dBlue flex items-center justify-center mb-4 md:mb-[30px]">
           <Star />
         </div>
-        <h1 tw="font-sans text-[28px] text-white font-bold">How did we do?</h1>
-        <p tw="text-lGrey mb-6">
+        <h1 tw="font-sans text-[28px] text-white font-bold mb-2 md:mb-4">
+          How did we do?
+        </h1>
+        <p tw="text-lGrey text-sm mb-4 md:mb-6">
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
@@ -72,7 +74,7 @@ export default function Card() {
   const SecondCard = () => (
     <div tw="flex flex-col items-center">
       <ThankYou tw="mb-6" />
-      <div tw="w-[193px] h-8 bg-dBlue rounded-full text-cOrange flex justify-center items-center mb-8">
+      <div tw="w-[193px] h-8 bg-dBlue rounded-full text-cOrange flex justify-center items-center mb-4 md:mb-8">
         You selected {active} out of 5
       </div>
       <h1 tw="font-sans font-bold text-white text-[28px] mb-2">Thank You!</h1>
